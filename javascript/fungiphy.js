9c659db22f10cafd150922b7e7885c6c39369211
pdmonge <parthamonge@gmail.com>
2017-08-27 18:45:22 -0600

    Basic Interface Done

---
+++ javascript/fungiphy.js
@@ -0,0 +1,10 @@
+// Use giphy API to retrieve interesting gifs
+
+// Initialize topic list - Actors
+var actorsList = ['Charlize Theron','Cate Blanchette','Anthony Hopkins','Ben Affleck'];
+var buttonsAreaDiv = $('#buttons-area');
+
+$(document).ready(function() {
+
+	$()
+});
